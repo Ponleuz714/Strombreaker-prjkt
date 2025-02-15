80f1df9d5a3b07ed64c68e3f0380d1485ec9e282
nurfinzmuhamad14 <mnurdiansyah105@gmail.com>
2025-02-15 21:46:44 +0000

    Import vendor tree 1/2

--- OplusKernelEnvConfig.mk
+++ OplusKernelEnvConfig.mk
@@ -132,7 +132,6 @@ KBUILD_CFLAGS += -DOPLUS_FEATURE_KTV
 KBUILD_CFLAGS += -DOPLUS_AUDIO_PA_BOOST_VOLTAGE
 
 $(foreach myfeature,$(ALLOWED_MCROS),\
-         $(warning make $(myfeature) to be a macro here) \
          $(eval KBUILD_CFLAGS += -D$(myfeature)) \
          $(eval KBUILD_CPPFLAGS += -D$(myfeature)) \
          $(eval CFLAGS_KERNEL += -D$(myfeature)) \
